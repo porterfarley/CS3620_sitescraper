d01ed2fd7332c14f7d3a8c9e1675d19f1d446c3d
Porter Farley <pfarley509@gmail.com>
2024-11-18 14:46:50 -0700

    Functionality for site scraper

--- sitescraper/scraper/views.py
+++ sitescraper/scraper/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+import requests
+from bs4 import BeautifulSoup
+from .models import Link
 
 # Create your views here.
+def scrape(request):
+    if request.method == "POST":
+        site = request.POST.get("site", "")
+    
+        page = requests.get(site)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        for link in soup.find_all('a'):
+            link_addresses = link.get('href')
+            link_text = link.string
+            Link.objects.create(address=link_addresses, name=link_text)
+            
+        return HttpResponseRedirect("/")
+    else:
+        data = Link.objects.all()
+        
+    return render(request, 'scraper/results.html', {'data':data})
+
+def clear(request):
+    Link.objects.all().delete()
+    data = Link.objects.all()    
+
+    return redirect(scrape)
